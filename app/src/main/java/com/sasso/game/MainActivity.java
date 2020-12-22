7f9331355a02b764f9fec8046656cdf0ec216a3f
Sasso <ricciuti.alessio@hotmail.com>
2020-12-22 13:21:58 +0100

    update

--- app/src/main/java/com/sasso/game/MainActivity.java
+++ app/src/main/java/com/sasso/game/MainActivity.java
@@ -3,12 +3,29 @@ package com.sasso.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        text = findViewById(R.id.text);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText("Niggers");
+            }
+        });
     }
+
+
 }
